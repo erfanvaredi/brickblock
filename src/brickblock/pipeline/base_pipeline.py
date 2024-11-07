7326a41583e836d6ed16338c17dc6dda9887e5ba
erfanvaredi <erfanvaredi@outlook.com>
2024-11-07 17:46:07 +0400

    feat: add support UUID

--- src/brickblock/pipeline/base_pipeline.py
+++ src/brickblock/pipeline/base_pipeline.py
@@ -19,6 +19,14 @@ from ..function import Function
 from ..abstract import BaseModule
 
 
+from json import JSONEncoder
+from uuid import UUID
+JSONEncoder_olddefault = JSONEncoder.default
+def JSONEncoder_newdefault(self, o):
+    if isinstance(o, UUID): return str(o)
+    return JSONEncoder_olddefault(self, o)
+JSONEncoder.default = JSONEncoder_newdefault
+
 class Pipeline:
     """
     A flexible pipeline system designed to chain together a sequence of synchronous and asynchronous functions
