542e853dfb7d639164fdf9526bae5cd56612d236
erfanvaredi <erfanvaredi@outlook.com>
2024-12-22 11:10:22 +0400

    feat: bump version to 0.1.23

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bb-core",
-    version="0.1.22",
+    version="0.1.23",
     description="Core package for the brickblock library.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
